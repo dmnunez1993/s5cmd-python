039614eb903d9c49233d454b75732a82591f828e
Diego Maldonado <dmnunez1993@cvlab.cs.tsukuba.ac.jp>
2025-05-12 16:46:55 +0900

    fix typo

--- s5cmdpy/s5cmd_runner.py
+++ s5cmdpy/s5cmd_runner.py
@@ -94,7 +94,7 @@ class S5CmdRunner:
             
 
         if self.no_sign_request:
-            args = ("--no-sign-request") + args
+            args = ("--no-sign-request",) + args
 
         if capture_output:
             try:
